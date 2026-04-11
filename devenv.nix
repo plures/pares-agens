b5c70749828a9b7308416c11332d87127727fc7c
Kayode Bristol <kayode.bristol@gmail.com>
2026-04-11 13:37:23 -0700

    added devenv; NIXOS

---
+++ devenv.nix
@@ -0,0 +1,40 @@
+{ pkgs, lib, ... }:
+
+{
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+  };
+
+  languages.javascript = {
+    enable = true;
+    pnpm.enable = true;
+  };
+
+  packages = with pkgs; [
+    nodejs
+    cargo-tauri
+    pkg-config
+    wrapGAppsHook4
+
+    webkitgtk_4_1
+    libsoup_3
+    gtk3
+    glib
+    gobject-introspection
+    librsvg
+    openssl
+  ];
+
+  env = {
+    WEBKIT_DISABLE_COMPOSITING_MODE = "1";
+  };
+
+  enterShell = ''
+    echo "rustc: $(rustc --version)"
+    echo "cargo: $(cargo --version)"
+    echo "node: $(node --version)"
+    echo "pnpm: $(pnpm --version)"
+    echo "tauri: $(cargo tauri --version)"
+  '';
+}
